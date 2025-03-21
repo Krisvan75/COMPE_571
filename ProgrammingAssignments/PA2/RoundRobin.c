324fde635ab758e32ab70b61ae36a4d8fa583c7e
krisvan75 <krisvanga75@gmail.com>
2025-03-20 23:04:19 -0700

    RoundRobin implementation added

--- ProgrammingAssignments/PA2/RoundRobin.c
+++ ProgrammingAssignments/PA2/RoundRobin.c
@@ -115,47 +115,64 @@ int main(int argc, char const *argv[])
 	running3 = 1;
 	running4 = 1;
 
-    struct timespec startTime, endTime[4];
-    clock_gettime(CLOCK_MONOTONIC, &startTime);
+    struct timespec startTime[4], endTime[4];
+	double responseTime[4];
 
 	while (running1 > 0 || running2 > 0 || running3 > 0 || running4 > 0)
 	{
 
+		
 
 		if (running1 > 0){
-
-            struct timespec Start, Stop;
-
-            clock_gettime(CLOCK_MONOTONIC, &Start);
+			clock_gettime(CLOCK_MONOTONIC, &startTime[0]);
 			kill(pid1, SIGCONT);
 			usleep(QUANTUM1);
 			kill(pid1, SIGSTOP);
-
+		}
+		if (waitpid(pid1, &running1, WNOHANG)>0) {
+			clock_gettime(CLOCK_MONOTONIC, &endTime[0]);
+			responseTime[0] = (endTime[0].tv_sec - startTime[0].tv_sec) + (endTime[0].tv_nsec - startTime[0].tv_nsec) / 1e9;
 		}
 		if (running2 > 0){
+			clock_gettime(CLOCK_MONOTONIC, &startTime[1]);
 			kill(pid2, SIGCONT);
 			usleep(QUANTUM2);
 			kill(pid2, SIGSTOP);
 		}
+		if (waitpid(pid2, &running2, WNOHANG)>0) {
+			clock_gettime(CLOCK_MONOTONIC, &endTime[1]);
+			responseTime[1] = (endTime[1].tv_sec - startTime[1].tv_sec) + (endTime[1].tv_nsec - startTime[1].tv_nsec) / 1e9;
+		}
 		if (running3 > 0){
+			clock_gettime(CLOCK_MONOTONIC, &startTime[2]);
 			kill(pid3, SIGCONT);
 			usleep(QUANTUM3);
 			kill(pid3, SIGSTOP);
 		}
+		if (waitpid(pid3, &running3, WNOHANG)>0) {
+			clock_gettime(CLOCK_MONOTONIC, &endTime[2]);
+			responseTime[2] = (endTime[2].tv_sec - startTime[2].tv_sec) + (endTime[2].tv_nsec - startTime[2].tv_nsec) / 1e9;
+		}
 		if (running4 > 0){
+			clock_gettime(CLOCK_MONOTONIC, &startTime[3]);
 			kill(pid4, SIGCONT);
 			usleep(QUANTUM4);
 			kill(pid4, SIGSTOP);
 		}
-		waitpid(pid1, &running1, WNOHANG);
-		waitpid(pid2, &running2, WNOHANG);
-		waitpid(pid3, &running3, WNOHANG);
-		waitpid(pid4, &running4, WNOHANG);
+		if (waitpid(pid4, &running4, WNOHANG)>0) {
+			clock_gettime(CLOCK_MONOTONIC, &endTime[3]);
+			responseTime[3] = (endTime[3].tv_sec - startTime[3].tv_sec) + (endTime[3].tv_nsec - startTime[3].tv_nsec) / 1e9;
+		}
+		
 	}
+
 
 	/************************************************************************************************
 		- Scheduling code ends here
 	************************************************************************************************/
+
+	double avg_response_time = (responseTime[0] + responseTime[1] + responseTime[2] + responseTime[3]) / 4.0;
+	printf("Average response time: %f\n", avg_response_time);
 
 	return 0;
 }
